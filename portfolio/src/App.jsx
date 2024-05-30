d649bafea5c1be53a6a3a708197069a916748d6c
Malik Umar <umargulzar2001@gmail.com>
2024-05-30 12:18:36 +0500

    just created the files for each component

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,7 +1,19 @@
+import { About } from "./components/about/About";
+import { Contact } from "./components/contacts/Contact";
+import { Footer } from "./components/footer/Footer";
+import { Home } from "./components/home/Home";
+import { Portfolio } from "./components/portfolio/Portfolio";
+import { Services } from "./components/services/Services";
+
 function App() {
   return (
     <>
-      <h1>MY Porfolio</h1>
+      <Home />
+      <About />
+      <Services />
+      <Portfolio />
+      <Contact />
+      <Footer />
     </>
   );
 }
