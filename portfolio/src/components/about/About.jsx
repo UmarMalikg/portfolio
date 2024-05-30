1d90ead826a5c21d9a5c3df8266e35b6fdce3ac6
Malik Umar <umargulzar2001@gmail.com>
2024-05-30 17:29:36 +0500

    about section designed

--- portfolio/src/components/about/About.jsx
+++ portfolio/src/components/about/About.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { AboutDP } from "./components/AboutDP";
 
 export const About = () => {
   return (
     <div id="about" className="about">
-      About
+      <div className="a-dp-section">
+        <AboutDP />
+      </div>
+      <div className="a-details-section">
+        <h1 className="a-title">
+          About <span>Me</span>
+        </h1>
+        <p className="a-description">
+          A Higher-Order Component (HOC) is a pattern in React that allows you
+          to enhance or extend the behavior of components by wrapping them with
+          a higher-order function. In simple terms, a HOC is a function that
+          takes a component as an input and returns a new component with
+          additional features or behavior.
+        </p>
+        <button className="a-read-more-btn">Read More</button>
+      </div>
     </div>
   );
 };
---
+++ portfolio/src/components/about/components/AboutDP.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import dp from "../../../assets/dp.png";
+
+export const AboutDP = () => {
+  return (
+    <div className="a-dp-holder">
+      <img className="a-dp" src={dp} alt="DP" />
+    </div>
+  );
+};
